95cab4c71f549ce2f760443bbe5c7ab543483880
Peter Širka <petersirka@gmail.com>
2014-04-06 11:28:49 +0200

    updated documentation

--- definitions/menu.js
+++ definitions/menu.js
@@ -4,5 +4,6 @@ framework.partial(function(next) {
 	self.repository.menuAdditional = self.module('docs').list('latest', 'Additional');
 	self.repository.menuPrototypes = self.module('docs').list('latest', 'Prototypes');
 	self.repository.menuBuilders = self.module('docs').list('latest', 'Builders');
+	self.repository.menuGlobals = self.module('docs').list('latest', 'Globals');
 	next();	
 });
