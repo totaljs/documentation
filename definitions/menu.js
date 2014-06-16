9b27d8185cea6340e474ab91a3f21c38177c40fe
Peter Širka <petersirka@gmail.com>
2014-06-16 20:40:18 +0200

    fixed: middleware

--- definitions/menu.js
+++ definitions/menu.js
@@ -1,9 +1,9 @@
-framework.partial(function(next) {	
+framework.middleware(function(next) {
 	var self = this;
 	self.repository.menu = self.module('docs').list('latest');
 	self.repository.menuAdditional = self.module('docs').list('latest', 'Additional');
 	self.repository.menuPrototypes = self.module('docs').list('latest', 'Prototypes');
 	self.repository.menuBuilders = self.module('docs').list('latest', 'Builders');
 	self.repository.menuGlobals = self.module('docs').list('latest', 'Globals');
-	next();	
+	next();
 });
