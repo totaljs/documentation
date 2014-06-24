57caef43d92a66878ebc295fdd2d87eb02011d23
Peter Širka <petersirka@gmail.com>
2014-06-24 21:43:37 +0200

    customized for new version

--- definitions/menu.js
+++ definitions/menu.js
@@ -1,9 +1,7 @@
-framework.middleware(function(next) {
-	var self = this;
+framework.on('controller', function(self, name) {
 	self.repository.menu = self.module('docs').list('latest');
 	self.repository.menuAdditional = self.module('docs').list('latest', 'Additional');
 	self.repository.menuPrototypes = self.module('docs').list('latest', 'Prototypes');
 	self.repository.menuBuilders = self.module('docs').list('latest', 'Builders');
 	self.repository.menuGlobals = self.module('docs').list('latest', 'Globals');
-	next();
 });
